c31541c593d54ec8756cab7b4d37aa08746f19fe
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-07-25 00:43:48 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-    double myPow(double x, int p) {
-        long long n=p;
-        if(x==1)
-            return 1;
-       if(n==0)
-           return 1;
-       if(n<0)
-       { double ans=1;
-           n=-n;
-           while(n!=0)
-           { 
-               if(n%2==0)
-               {
-                   n=n/2;
-                   x=x*x;
-               }
-               else
-               {ans= x*ans;
-               n--;}
-           }
-        return 1/ans;
-       }
-        else   
-       { double ans=1;
-            while(n!=0){
-           if(n%2==0)
-               {
-                   n=n/2;
-                  x=x*x;
-               }
-               else
-               {ans= x*ans;
-               n--;}
-            }
-        return ans;
-       }
-        
+    double myPow(double x, long long p) {
+         if(p==0)
+             return 1;
+        if(p==1)
+            return x;
+        if(p<0)
+        {
+            return 1.0/myPow(x,-p);
+        }
+        else
+        {
+        if(p%2==0)
+        {
+            return myPow(x*x,p/2);
+        }
+        else
+        {
+            return x* myPow(x*x,p/2);
+        }
+        }
         
     }
 };
