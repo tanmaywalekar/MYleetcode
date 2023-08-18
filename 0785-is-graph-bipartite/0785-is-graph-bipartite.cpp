759a753a23984bc3c529e6bcd858b8014edb7aa7
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-08-19 01:06:50 +0530

    Time: 19 ms (85.54%), Space: 13.4 MB (70.21%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    bool dfs(int i,int p,vector<vector<int>>& g,vector<int> &v,vector<int>&cr,int r)
+    bool isoddcycle(int i,int p,vector<vector<int>>& graph,vector<int> &v,vector<int> &clr,int cl)
     {
         v[i]=1;
-        cr[i]=r;
-        bool ans=false;
-        for(auto c:g[i])
+        clr[i]=cl;
+       bool ans=false;
+        for(auto c:graph[i])
         {
             if(v[c]==0)
-            {
-                ans=ans|dfs(c,i,g,v,cr,r^1);
+            {  
+               ans= ans|isoddcycle(c,i,graph,v,clr,cl^1);
             }
             else
             {
-                
-                 if(c!=p){
-                     if(cr[i]==cr[c])
-                    return true;}
-                
+                if(c!=p)
+                {
+                    if(clr[i]==clr[c])
+                        return true;
+                   
+                }
             }
         }
         return ans;
     }
     bool isBipartite(vector<vector<int>>& g) {
-        vector<int>v(g.size(),0);
-        vector<int>c(g.size(),-1);
+        
+        
+        vector<int> v(g.size(),0);
+        vector<int> clr(g.size(),-1);
         for(int i=0;i<g.size();i++)
         {
             if(v[i]==0)
             {
-                if(dfs(i,-1,g,v,c,0))
-                    return false;
+             if(isoddcycle(i,-1,g,v,clr,0))
+             {
+                 return false;
+             }
             }
         }
         return true;
