dbbc4d930af16f1da4b85899a5c68280225eda74
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-08-22 00:50:56 +0530

    Time: 60 ms (26.33%), Space: 91.6 MB (21.92%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string x="";
+        for(int i=0;i<s.size()-1;i++)
+        { 
+            x.push_back(s[i]);
+            if(s.size()%(i+1)==0)
+            {
+                
+                int z=0;
+              
+                string y="";
+                while(1)
+                {
+                    y+=x;
+                    z+=i+1;
+                    if(z==s.size())
+                        break;
+                }
+                if(y==s)
+                { //cout<<y<<" "<<x<<" "<<z<<" "<<" "<<i+1;
+                    return true;}
+            }
+        }
+        return false;
+    }
+};
