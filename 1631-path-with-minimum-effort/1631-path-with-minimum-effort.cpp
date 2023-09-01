ca6b2017856d782e50068e204c2b2c6561cc0e0a
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-09-02 03:06:58 +0530

    Time: 490 ms (8.71%), Space: 69.7 MB (6.66%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> d{0,-1,0,1,0};
+    bool check(int x,vector<vector<int>>& h)
+    {
+        vector<vector<int>> v(h.size(),vector<int>(h[0].size(),0));
+        queue<pair<int,int>>q;
+        v[0][0]=1;
+        q.push({0,0});
+        while(q.size())
+        {
+            int i=q.front().first;
+            int j=q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++)
+            {
+                if(i+d[k]>=0&&i+d[k]<h.size()&&j+d[k+1]>=0&&j+d[k+1]<h[0].size()&&v[i+d[k]][j+d[k+1]]==0&&(abs(h[i][j]-h[i+d[k]][j+d[k+1]])<=x))
+                {
+                    v[i+d[k]][j+d[k+1]]=1;
+                    q.push({i+d[k],j+d[k+1]});
+                }
+            }
+        }
+        if(v[h.size()-1][h[0].size()-1])
+            return true;
+        else
+            return false;
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int l=0;
+        int h=1e6;
+        int ans=h;
+        while(l<=h)
+        {
+            int m=l+(h-l)/2;
+            if(check(m,heights))
+            {
+                ans=min(ans,m);
+                h=m-1;
+            }
+            else
+                l=m+1;
+        }
+        return ans;
+    }
+};
