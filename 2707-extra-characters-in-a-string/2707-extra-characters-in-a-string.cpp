a246771dc8a1b545beb75e638e883339c3ffb6f7
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-09-03 03:51:08 +0530

    Time: 2519 ms (5.04%), Space: 342.3 MB (5.04%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    unordered_set<string>s;
+    int dp[51][51];
+    int rec(int i,int j,string &z)
+    {
+        if(s.find(z.substr(i,j-i+1))!=s.end())
+            return 0;
+        if(i==j)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int ans=z.length();
+        for(int k=i;k<j;k++)
+        {
+            int b=rec(i,k,z)+rec(k+1,j,z);
+            ans=min(ans,b);
+            
+        }
+        return dp[i][j]= ans;
+    }
+    int minExtraChar(string st, vector<string>& d) {
+        for(auto &i:d)
+        {
+            s.insert(i);
+        }
+        memset(dp,-1,sizeof(dp));
+        return rec(0,st.size()-1,st);
+    }
+};
