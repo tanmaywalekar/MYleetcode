5854ae49fb9473014aaf964afc4d866581d0bf30
TANMAY WALEKAR <76596797+tanmaywalekar@users.noreply.github.com>
2023-09-03 03:56:54 +0530

    Time: 154 ms (63.81%), Space: 80.4 MB (58.93%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
-    unordered_set<string>s;
-    int dp[51][51];
-    int rec(int i,int j,string &z)
-    {
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s.find(z.substr(i,j-i+1))!=s.end())
-            return 0;
-        if(i==j)
-            return 1;
-        
-        int ans=z.length();
-        for(int k=i;k<j;k++)
-        {
-            int b=0;
-            if(dp[i][k]!=-1)
-                b+=dp[i][k];
-            else
-                b+=rec(i,k,z);
-            
-             if(dp[k+1][j]!=-1)
-                b+=dp[k+1][j];
-            else
-                b+=rec(k+1,j,z);
-              
-            ans=min(ans,b);
-            
-        }
-        return dp[i][j]= ans;
-    }
     int minExtraChar(string st, vector<string>& d) {
-        for(auto &i:d)
-        {
-            s.insert(i);
+        unordered_set<string> s;
+    for (auto& i : d) {
+        s.insert(i);
+    }
+    
+    int n = st.size();
+    int dp[51][51];
+    
+    // Initialize dp table
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = -1;
         }
-        memset(dp,-1,sizeof(dp));
-        return rec(0,st.size()-1,st);
+    }
+    
+    // Base case: single characters are already present in the dictionary
+    for (int i = 0; i < n; i++) {
+        if (s.find(string(1, st[i])) != s.end()) {
+            dp[i][i] = 0;
+        } else {
+            dp[i][i] = 1;
+        }
+    }
+    
+    // Fill in the dp table
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; i++) {
+            int j = i + len - 1;
+            dp[i][j] = len; // Initialize to worst case
+            
+            // Check if the entire substring is in the dictionary
+            if (s.find(st.substr(i, len)) != s.end()) {
+                dp[i][j] = 0;
+                continue;
+            }
+            
+            // Try to split the substring and minimize the result
+            for (int k = i; k < j; k++) {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j]);
+            }
+        }
+    }
+    
+    return dp[0][n - 1];
+        
     }
 };
